0ab6ec23f8c3d0160241fde826a8861fda09188c
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-02-06 16:20:00 +0300

    spread

---
+++ повторение/script.js
@@ -0,0 +1,31 @@
+// Практика - Деструктуризация и операторы spread/rest:
+// 1. Напишите функцию, которая принимает объект и возвращает новый объект, где ключи и значения поменяны местами.
+
+// Ожидаемый результат: { 1: 'a', 2: 'b', 3: 'c' }
+
+function swapKeysAndValues(obj) {
+  const swapped = {};
+  for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+          swapped[obj[key]] = key;
+      }
+  }
+  return swapped;
+}
+
+// Пример использования
+const obj = { a: 1, b: 2, c: 3 };
+const swapped = swapKeysAndValues(obj);
+console.log(swapped); 
+
+
+
+
+
+// 2. Создайте массив, объединив два других массива с помощью spread
+
+const arrayOne = [1, 2, 30]
+const arrayTwo = [9,85,42]
+const mainArray = [...arrayOne, ...arrayTwo]
+console.log(mainArray);
+
