cb1f3fb14c8ef9fc4ec5d7afc6bf048c9bb3abf4
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-08-07 17:42:43 +0300

    lesson three

--- повторение решение/lesson-03/01.js
+++ повторение решение/lesson-03/01.js
@@ -1,3 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(fisrt, second) {
+  return Math.max(a, b, c);
+}
+
+console.log(calculateAverage(5, 10));
+
--- повторение решение/lesson-03/03.js
+++ повторение решение/lesson-03/03.js
@@ -1,3 +1,14 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(fisrt, second, third) {
+if (fisrt > second && fisrt > third) {
+  return fisrt 
+} else if (second > fisrt && second > third){
+    return second 
+} else {
+  return third
+}
+}
+
+console.log(findLargest(15, 5, 9));
+
